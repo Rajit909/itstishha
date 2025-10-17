69af24fd37d151a45dbba5bd6f731f44a5aa10a4
Rajit Kumar <krajit909@gmail.com>
2025-10-17 05:25:58 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/components/hero-animation-client.tsx
+++ src/components/hero-animation-client.tsx
@@ -1,17 +1,10 @@
 
 "use client";
 
-import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 
 const HeroAnimation = dynamic(() => import('@/components/hero-animation'), { ssr: false });
 
 export default function HeroAnimationClient() {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  return <>{isClient && <HeroAnimation />}</>;
+  return <HeroAnimation />;
 }
